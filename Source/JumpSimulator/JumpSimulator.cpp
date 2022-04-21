a91c7ad059d136ed4d0b2b9c55f32c00cd6a2527
PLANCQ Valentin <sulfeur84@gmail.com>
2022-04-21 16:54:36 +0200

    Le jeu

---
+++ Source/JumpSimulator/JumpSimulator.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JumpSimulator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JumpSimulator, "JumpSimulator" );
---
+++ Source/JumpSimulator/MyCharacter.cpp
@@ -0,0 +1,55 @@
+// Implémentations
+
+
+#include "MyCharacter.h"
+#include <Runtime/Engine/Classes/Kismet/GameplayStatics.h>
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AMyCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AMyCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &AMyCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AMyCharacter::AddControllerPitchInput);
+	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, &AMyCharacter::Jump);
+	PlayerInputComponent->BindAction(TEXT("Restart"), IE_Pressed, this, &AMyCharacter::RestartLevel);
+}
+
+void AMyCharacter::MoveForward(float AxisVal)
+{
+	AddMovementInput(GetActorForwardVector() * AxisVal);
+}
+
+void AMyCharacter::MoveRight(float AxisVal)
+{
+	AddMovementInput(GetActorRightVector() * AxisVal);
+}
+
+void AMyCharacter::RestartLevel() 
+{
+	UGameplayStatics::OpenLevel(this, FName(*GetWorld()->GetName()), false);
+}
+
